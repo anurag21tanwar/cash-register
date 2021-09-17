bf3b0e978e51b796830a3590215a7d061bc19f5b
Anurag Tanwar <anurag21tanwar@gmail.com>
2021-09-17 13:24:17 +0800

    Setting up application

---
+++ app/javascript/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default () => (
+    <div>
+        <nav className="navbar navbar-light bg-light">
+            <div className="container-fluid">
+                <span className="navbar-brand mb-0 h1">Anurag's Cash Register HW</span>
+            </div>
+        </nav>
+    </div>
+);
