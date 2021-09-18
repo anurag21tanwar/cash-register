1a205822193fbe8baeed2f1f6852cf384b9a8a19
Anurag Tanwar <anurag21tanwar@gmail.com>
2021-09-18 19:10:49 +0800

    Deploy to heruko

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,11 +3,26 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
-import "channels"
+import Rails from "@rails/ujs";
+import Turbolinks from "turbolinks";
+import * as ActiveStorage from "@rails/activestorage";
+import "channels";
+import React from "react";
+import { render } from "react-dom";
+import $ from 'jquery';
+import Popper from 'popper.js';
+import App from "../components/App";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+document.addEventListener("DOMContentLoaded", () => {
+  render(
+    <App />,
+    document.body.appendChild(document.createElement("div"))
+  );
+});
